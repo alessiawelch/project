e3501c0594d85e6b2dcdc47a8cc96b4318354008
alessiawelch <96654070+alessiawelch@users.noreply.github.com>
2025-01-11 14:51:00 +0000

    Update run-gsage-mean-2-6.py

--- run-gsage-mean-2-6.py
+++ run-gsage-mean-2-6.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     task = Task.NEIGHBORS_MATCH
     gnn_type = GNN_TYPE.GSAGE_MEAN
     stopping_criterion = STOP.TRAIN
-    min_depth = 3
-    max_depth = 3
+    min_depth = 4
+    max_depth = 4
 
     results_all_depths = {}
     for depth in range(min_depth, max_depth + 1):
