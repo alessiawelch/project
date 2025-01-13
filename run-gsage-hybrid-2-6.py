9b8989ed78cfa9b6618344a4361ada610daba592
alessiawelch <96654070+alessiawelch@users.noreply.github.com>
2025-01-13 19:40:13 +0000

    Update run-gsage-hybrid-2-6.py

--- run-gsage-hybrid-2-6.py
+++ run-gsage-hybrid-2-6.py
@@ -23,10 +23,10 @@ class Results:
 if __name__ == '__main__':
 
     task = Task.NEIGHBORS_MATCH
-    gnn_type = GNN_TYPE.GSAGE_HYBRID
+    gnn_type = GNN_TYPE.GSAGE_MAXSUM
     stopping_criterion = STOP.TRAIN
     min_depth = 5
-    max_depth = 6
+    max_depth = 5
 
     results_all_depths = {}
     for depth in range(min_depth, max_depth + 1):
